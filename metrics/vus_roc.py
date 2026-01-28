b0e4eb7f6378880d348ac30199c1480d643ef092
infogroup502 <energysmartcontrol@126.com>
2026-01-28 17:22:08 +0800

    Add files via upload

---
+++ metrics/vus_roc.py
@@ -0,0 +1,23 @@
+from utils.metrics import metricor
+import numpy as np
+
+def generate_curve(label, score, slidingWindow):
+    tpr_3d, fpr_3d, prec_3d, window_3d, avg_auc_3d, avg_ap_3d = metricor().RangeAUC_volume(labels_original=label,
+                                                                                           score=score,
+                                                                                           windowSize=1 * slidingWindow)
+    X = np.array(tpr_3d).reshape(1, -1).ravel()
+    X_ap = np.array(tpr_3d)[:, :-1].reshape(1, -1).ravel()
+    Y = np.array(fpr_3d).reshape(1, -1).ravel()
+    W = np.array(prec_3d).reshape(1, -1).ravel()
+    Z = np.repeat(window_3d, len(tpr_3d[0]))
+    Z_ap = np.repeat(window_3d, len(tpr_3d[0]) - 1)
+
+    return Y, Z, X, X_ap, W, Z_ap, avg_auc_3d, avg_ap_3d
+
+def get_range_vus_roc(score, labels, slidingWindow):
+    grader = metricor()
+    R_AUC_ROC, R_AUC_PR, _, _, _ = grader.RangeAUC(labels=labels, score=score, window=slidingWindow, plot_ROC=True)
+    _, _, _, _, _, _,VUS_ROC, VUS_PR = generate_curve(labels, score, 2*slidingWindow)
+    metrics = {'R_AUC_ROC': R_AUC_ROC, 'R_AUC_PR': R_AUC_PR, 'VUS_ROC': VUS_ROC, 'VUS_PR': VUS_PR}
+
+    return metrics
